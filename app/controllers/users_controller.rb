42201ad399c6a8d61068ae2f4005abaa4692d235
Calvin <Calvin.lacc@gmail.com>
2016-09-04 16:46:12 -0600

    Able to create users from the homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      rerender :new
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :id)
+  end
 end
